595d07bc0a6a2bc1a3b406258224cd6a2adc4706
Altoids?! <38045884+CoderSudaWuda@users.noreply.github.com>
2022-07-06 10:36:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 const socket = new WebSocket('wss://n6q2b6jhnn8gmgac.uvwx.xyz:5000/?a=2', ["arras.io#v0+ft2", "arras.io#v1+ft2"]);
 
-socket.onmessage = function({ data }) { console.log('d') }
+socket.onmessage = function({ data }) { console.log(Array.from(data).toString()) }
 socket.onerror = function() { console.log('e') }
 socket.onclose = function() { console.log('c') }
